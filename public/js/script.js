dd337bf208608d7da156c643c05e469c3a7753a5
mrmoi <mlmart16@asu.edu>
2016-10-27 22:47:59 -0700

    firebase init

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,26 @@
-$('.carousel').carousel({
-    interval: 3000
+$(document).ready(function(){
+
+/*document.write("Hello, World!");*/
+
+/*    $("#mydiv").scroll(function(){
+        if($(this)[0].scrollHeight - $(this).scrollTop() <= $(this).outerHeight())
+        {
+            alert(1);
+        }
+
+        if($(this).scrollTop() == 0)
+        {
+            alert(2);
+        }
+    });
+*/
+/*$("p").css("background-color", "yellow");*/
+
+
+
+
+
+
+
+
 });
