5ba2d9d324964bd9e2ea3f168094a214d18af65a
Moises Martinez <mlmart16@asu.edu>
2016-10-29 19:54:26 -0700

    Fixed BackendController scope issue

--- public/js/controllers/backend.js
+++ public/js/controllers/backend.js
@@ -1,21 +1,15 @@
 myApp.controller('BackendController',['$scope','$rootScope','$firebaseAuth','$firebaseArray','FIREBASE_URL',
 function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {
 
-    var ref = new Firebase(FIREBASE_URL);
-
     /* NAME FIELD*/
     var nameRef = new Firebase(FIREBASE_URL + '/name');
     var nameInfo = $firebaseArray(nameRef);
-    $scope.name = nameInfo;
+    $scope.names = nameInfo;
 
     /* ABOUT ME CONTENT */
     var bioRef = new Firebase(FIREBASE_URL + '/bio');
     var bioInfo = new $firebaseArray(bioRef);
-    $scope.bio = bioInfo;
-
-
-
-
+    $scope.bios = bioInfo;
 
 
     /* NAME FIELD FUNCTION */
@@ -33,7 +27,7 @@ function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {
         bioInfo.$add({
             about: $scope.aboutField,
             date: Firebase.ServerValue.TIMESTAMP
-        }).then(function () {
+        }).then(function() {
             $scope.aboutField='';
         });
     };
@@ -46,52 +40,3 @@ function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {
 
 
 
-
-
-
-
-
-/*
-######## CODE SNIPPETS ########
-
- var auth = $firebaseAuth(ref);
-
- auth.$onAuth(function(authUser) {
- if(authUser) {
-
- playersInfo.$loaded().then(function(data) {
- $rootScope.howManyPlayers = playersInfo.length;
- });
-
- playersInfo.$watch(function(data) {
- $rootScope.howManyPlayers = playersInfo.length;
- });
-
- /*} // User Authenticated
- }); // on Auth
-
- $scope.addPlayer = function() {
- playersInfo.$add({
- name: $scope.playername,
- position: $scope.playerposition,
- /*nationality: $scope.playernationality,
- number: $scope.playernumber,
- status: $scope.playerstatus,
- date: Firebase.ServerValue.TIMESTAMP
- }).then(function() {
- $scope.playername='';
- $scope.playerposition='';
- /*$scope.playernationality='';
- $scope.playernumber='';
- $scope.playerstatus='';
- });
- };
-
- $scope.deletePlayer = function(key) {
- playersInfo.$remove(key);
- }; // deleteMeeting
-
-
-
- ######## CODE SNIPPETS ########
-*/
