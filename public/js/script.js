6735ba3d51675cfd3bea3f0c2550224737fc9402
mrmoi <mlmart16@asu.edu>
2016-10-31 00:20:29 -0700

    navbar on scroll

--- public/js/script.js
+++ public/js/script.js
@@ -19,6 +19,15 @@ $(document).ready(function(){
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+/*
+$(window).scroll(function () {
+    $("nav").css("background-color", "black");
+})
+*/
+ $(document).on( 'scroll', 'nav', function() {
+    $("nav").css("background-color", "black");
+     console.log('Event Fired');
+ })
 
 
 
