3ab9e88586b41496e9a27bc502fa69eb3b2c2ba3
mrmoi <mlmart16@asu.edu>
2016-11-02 12:00:21 -0700

    navbar scroll effect

--- public/js/script.js
+++ public/js/script.js
@@ -21,10 +21,26 @@ $(function () {
 })
 
 
-$(window).scroll(function () {
-    $("nav").css("background-color", "#A0A0A0");
-    $("nav").css("border-bottom", "2px solid black");
-    $("#bg").css("opacity", "0.9");
-})
+//$(window).scroll(function () {
+//    $("nav").css("background-color", "#A0A0A0");
+//    $("nav").css("border-bottom", "2px solid black");
+//    $("#bg").css("opacity", "0.9");
+//})
+
+    $(window).on('scroll', function() {
+        var scrollTop = $(this).scrollTop();
+
+        $('.aboutContainer').each(function() {
+            var topDistance = $(this).offset().top;
+
+            if ( (topDistance+10) < scrollTop ) {
+                //alert( $(this).text() + ' was scrolled to the top' );
+                $("nav").css("background-color", "#A0A0A0");
+            } else {
+                $("nav").css("background-color", "transparent");
+            }
+        });
+    });
+
 
 });
